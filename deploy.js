3c08e994786fdfb64cbf9d044eecc24aa08e33ab
Maxime Thirouin <m@moox.io>
2014-11-16 09:39:42 +0100

    gh-pages deploy should be ready this way

--- deploy.js
+++ deploy.js
@@ -1,7 +1,14 @@
 console.log("@todo test & enable deploy task")
 process.exit(1)
 
-require("gh-pages").publish(require("path").join(__dirname, "dist"), function(err){
-  console.error(err)
-  process.exit(1)
-})
+require("gh-pages").publish(
+  require("path").join(__dirname, "dist"),
+  {
+    repo : "https://" + (process.env.GH_TOKEN ? process.env.GH_TOKEN + "@" : "") + "github.com/" + process.env.GH_OWNER + "/" + process.env.GH_PROJECT_NAME + ".git",
+    silent : true
+  },
+  function(err){
+    console.error(err)
+    process.exit(1)
+  }
+)
