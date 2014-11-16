5232610fe23f34e311753f96fcb73e52902b4cc2
Matthias Le Brun <mlbli@me.com>
2014-11-16 13:47:32 +0100

    section component

---
+++ src/components/common/section.js
@@ -0,0 +1,19 @@
+var React = require("react/addons")
+
+var Title = require("./title")
+
+var Section = React.createClass({
+  propTypes : {
+    title : React.PropTypes.title.string
+  },
+  render() {
+    <div className="putainde-Section r-Grid">
+      <div className="putainde-Section-contents r-Grid-cell r-all--8of12">
+        <Title title={this.props.title}/>
+        {this.props.children}
+      </div>
+    </div>
+  }
+})
+
+module.exports = Section
