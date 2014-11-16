c3ed408a75b34a418fbee77092a944c19b67f1fe
Matthias Le Brun <mlbli@me.com>
2014-11-16 15:36:43 +0100

    ensure actionTypes are well passed

--- src/dispatcher/index.js
+++ src/dispatcher/index.js
@@ -3,12 +3,18 @@ var Dispatcher = require("flux").Dispatcher
 
 var AppDispatcher = assign(Dispatcher.prototype, {
   handleServerAction(action) {
+    if(action.type == null) {
+      console.error("action" , action, " has an undefined type")
+    }
     this.dispatch({
       source : "SERVER",
       action : action
     })
   },
   handleViewAction(action) {
+    if(action.type == null) {
+      console.error("action" , action, " has an undefined type")
+    }
     this.dispatch({
       source : "VIEW",
       action : action
