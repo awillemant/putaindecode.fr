4d04fc27d6a01a1f41feb63e4b2f9dcc7bd2e1e6
Matthias Le Brun <mlbli@me.com>
2014-11-21 17:16:48 +0100

    added namespaced attribute to <use> component

--- src/components/svg/use.js
+++ src/components/svg/use.js
@@ -1,13 +1,11 @@
 var React = require("react/addons")
 
-var XLINK_HREF_ATTRIBUTE = "xlink:href"
-
 var Use = React.createClass({
   propTypes : {
     xlinkHref : React.PropTypes.string.isRequired
   },
   componentDidMount() {
-    this.getDOMNode().setAttribute(XLINK_HREF_ATTRIBUTE, this.props.xlinkHref)
+    this.getDOMNode().setAttributeNS("http://www.w3.org/1999/xlink", "href", this.props.xlinkHref)
   },
   render() {
     return React.createElement("use", null)
